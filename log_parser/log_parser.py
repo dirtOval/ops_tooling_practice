41ce4291821d7ffd888ebd92147cc9d0e6e8f6ee
Deirdre <deirdre.loyall@gmail.com>
2026-07-12 16:21:44 -0400

    can now find service with most errors (weighted by severity) with -m flag

--- log_parser/log_parser.py
+++ log_parser/log_parser.py
@@ -22,14 +22,50 @@ def count_by_level(file, service=None):
         raise KeyError(f'Error level not found: {log_level}')
   return log_count
 
+def find_most_error_service(file: file):
+  services: dict = {}
+  with open(file) as f:
+    for x in f:
+      log = json.loads(x)
+      rating = rate_severity(log['level'])
+      if log['service'] in services:
+        services[log['service']] += rating
+      else:
+        services[log['service']] = rating
+  winner = max(services, key=lambda service: services[service])
+  return winner
+
+def rate_severity(level: str):
+  ratings: dict = {
+    'DEBUG': 1,
+    'INFO': 2,
+    'WARN': 3,
+    'ERROR': 4,
+    'CRITICAL': 5,
+  }
+  return ratings[level]
+
 def main():
   parser = argparse.ArgumentParser(description="Count error logs by severity level")
   parser.add_argument('-f', '--file', type=str, default=None, help='The log file to be parsed' )
   parser.add_argument('-s', '--service', type=str, nargs='*', default=None, help='The service(s) to be monitored.')
+  parser.add_argument('-m', '--most', action='store_true', help='Returns the service with the most errors, weighted by severity')
   args = parser.parse_args()
+  if not args.file:
+    raise TypeError('No log file specified.')
+
+  output_text: str = ''
+
+  if args.most:
+    most = find_most_error_service(args.file)
+    count = count_by_level(args.file, most)
+    output_text += f'{most} has the most error messages.\n'
+    for x in count.keys():
+      output_text += f'{x}: {count[x]}\n'
+    print(output_text)
+    return
 
   result = count_by_level(args.file, args.service)
-  output_text: str = ''
   if args.service:
     output_text += 'services monitored: '
     for service in args.service: output_text += f'{service} '
